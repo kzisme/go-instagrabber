79028d44931accf466fd5bd64b058fd04a9cd455
Kevin Zita <bleedgreenandgold@gmail.com>
2020-10-25 01:54:43 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fileName := "test.jpg"
+	URL := ""
+	err := downloadImage(URL, fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("File %s downloaded in current working directory", fileName)
+}
+
+func downloadImage(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
+	}
+	//Create a empty file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//Write the bytes to the file
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
